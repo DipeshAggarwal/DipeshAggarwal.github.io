2a3aa1d2c14f71720412fe4488b6a242e1be0b66
Dipesh Aggarwal <DipeshAggarwal@users.noreply.github.com>
2020-04-01 15:27:46 +0530

    Query striings now also take column index as keys
    * Allow width to be dynimacally calculated
    * Bettter support for columns with very long text
    * Better support for tables with less number of columns
    * Allow quotes in dropdown.

--- covid-order/script.js
+++ covid-order/script.js
@@ -4,12 +4,16 @@ $(document).ready( function () {
     var pageLength = 15;
     var queryArray = [""];
     const urlParams = new URLSearchParams(window.location.search);
+    var columnBreakpoints = [ "meddesktop", "tabletp", "mobilel", "mobilep" ];
 
+    // Check if a custom sheet query is provided
     if ( urlParams.get("sheet") ) {
         sheetID = urlParams.get("sheet");
     } else {
         sheetID = "19PBIps8-1k4jEjEv8iRH6DQp1IsVZlYCBwninqSxYNg";
     };
+
+    // Check if a custom worksheet query is provided
     if ( urlParams.get("page") ) {
         sheetName = urlParams.get("page");
     } else {
@@ -32,10 +36,27 @@ $(document).ready( function () {
         
         sheetData = data[sheetName].elements;
         columnObj = [{"orderable": false,"data": null,"defaultContent": ''}];
-        for (var name of columnNames) {
-            $("#firstRow").append("<th>" + name +"</th>")
-            columnObj.push({"mDataProp": name})
-        }
+        columnDefs = [{ "width": "5%", "targets": 0 }];
+
+        // Here we separate the data column wise to feed to DataTable
+        for (var i = 0; i < columnNames.length; i++) {
+            columnObj.push( {"mDataProp": columnNames[i]} );
+
+            // The first five columns are given fixed width
+            if (i < 5) {
+                columnDefs.push({ "width": "20%", "targets": i });
+            };
+
+            // Add responsive breakpoints to the first four columns
+            // And add "none" class to the remaining columns so that they don't show
+            if (columnBreakpoints.length > 0) {
+                $("#firstRow").append('<th class="' + columnBreakpoints.join(' ') + '">' + columnNames[i] +'</th>');
+                columnBreakpoints.pop();
+            } else {
+                $("#firstRow").append('<th class="none">' + columnNames[i] +"</th>");
+            }
+        };
+        columnDefs.push({className: 'control', orderable: false,targets: 0});
 
         var table = $('#orderTable').DataTable({
             "responsive": {
@@ -44,6 +65,7 @@ $(document).ready( function () {
                     target: 'tr',
                     renderer: function ( api, rowIdx, columns ) {
                         var data = $.map( columns, function ( col, i ) {
+                            // We customise how we want to show the data when the plus sign is clicked
                             if (col.hidden && col.data) {
                                 if (col.data.includes("http")) {
                                     return '<tr data-dt-row="'+col.rowIndex+'" data-dt-column="'+col.columnIndex+'">'+
@@ -73,36 +95,25 @@ $(document).ready( function () {
             "bProcessing": true,
             "data": sheetData,
             "aoColumns": columnObj,
-            "aoColumnDefs": [
-                { "width": "5%", "targets": 0 },
-                { "width": "20%", "targets": 1 },
-                { "width": "20%", "targets": 2 },
-                { "width": "20%", "targets": 3 },
-                { "width": "20%", "targets": 4 },
-                {
-                    className: 'control',
-                    orderable: false,
-                    targets:   0
-                }
-            ],
+            "aoColumnDefs": columnDefs,
             "order": [[0, null]],
             "breakpoints": [
-              {name: 'bigdesktop', width: Infinity},
-              {name: 'meddesktop', width: 1480},
-              {name: 'smalldesktop', width: 1280},
-              {name: 'medium', width: 1188},
-              {name: 'tabletl', width: 1024},
-              {name: 'btwtabllandp', width: 848},
-              {name: 'tabletp', width: 768},
-              {name: 'mobilel', width: 480},
-              {name: 'mobilep', width: 320}
+                {name: 'bigdesktop', width: Infinity},
+                {name: 'meddesktop', width: 1480},
+                {name: 'smalldesktop', width: 1280},
+                {name: 'medium', width: 1188},
+                {name: 'tabletl', width: 1024},
+                {name: 'btwtabllandp', width: 848},
+                {name: 'tabletp', width: 768},
+                {name: 'mobilel', width: 480},
+                {name: 'mobilep', width: 320}
             ],
             "fnCreatedRow": function( nRow, aData, iDataIndex ) {
-                value = aData[columnObj[1].mDataProp].replace(/\s+/g,' ').trim();
+                value = aData[columnObj[1].mDataProp].replace(/\s+/g,' ').replace(/'/g, "\\'").trim();
                 if ( $("#state-box option[value='" + value + "']").length == 0 ) {
                     $('<option/>').val(value).html(value).appendTo('#state-box');
                 };
-                dataArray = aData[columnObj[2].mDataProp].replace(/\s+/g,' ').trim().split(",");
+                dataArray = aData[columnObj[2].mDataProp].replace(/\s+/g,' ').replace(/'/g, "\\'").trim().split(",");
                 dataArray.forEach(function(entry) {
                     if ( $("#issue-box option[value='" + entry.trim() + "']").length == 0 ) {
                         $('<option/>').val(entry.trim()).html(entry.trim()).appendTo('#issue-box');
@@ -145,11 +156,17 @@ $(document).ready( function () {
 
                 table.column(index).search(value).draw();
 
-                if (key === "state") {
+                /*if (key === "state") {
                     $("#state-box option[value="+value+"]").attr('selected', 'selected');
+                }*/
+            } else if ( !(isNaN(key)) ) {
+                index = parseInt(key);
+                if (key < queryArray.length) {
+                    value = urlParams.get(key);
+                    table.column(index).search(value).draw();
                 }
             }
-        }
+        };
 
         $("#loader").hide();
         $("#full-container").show();
